2d94d3fe83fd32cb16fbd20fb3f2dcbeb494a41c
Andrew Bruckbauer <49802191+ShekelsBot@users.noreply.github.com>
2021-12-12 10:44:55 -0700

    Update Campus_Tickets_Bruckbauer2sql.sql
    Updated relation ships

--- Campus_Tickets_Bruckbauer2sql.sql
+++ Campus_Tickets_Bruckbauer2sql.sql
@@ -1,86 +1,119 @@
--- ANDREW BRUCKBAUER
--- 10/21/2021
--- PART ONE -- CREATE DATABASE AND DELTE ONE TABLE
+-- Andrew Bruckbauer
+-- 12/12/2021
+-- ParkingBot DB
 
-create database CampusTickets; 
-	use CampusTickets; 
-    -- Use the campus tickets database --
-    create table Theater(Name varchar(10));
-    -- Create a table named theatre with one column so it is valid --
-    drop table Theater;
-    -- Drop the table --
-    
--- PART TWO -- CREATE THREE TABLES AND DEFINE RELATIONSHIPS
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-    create table Student(
-		StudentID int,
-        -- Primary key is int --
-        FirstName varchar(30),
-        LastName varchar(30),
-        PhoneNum int,
-        StateLicense varchar(20),
-        LicenseNo int,
-        Primary Key (StudentID));
-        -- Define Student ID as primary key --
-    
-        create table TicketCode(
-		code int,
-        fine varchar(10),
-        Primary key (code));
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema parkingspots
+-- -----------------------------------------------------
 
--- Needs to be created last becuase its a weak entity    
-    create table Ticket(
-		TicketNo int,
-        -- Primary key
-		Date varchar(10),
-        StudentID int,
-        -- match primary key from Student --
-        Code int,
-        Primary key (TicketNo),
-        Foreign Key (StudentID) references Student(StudentID),
-        Foreign Key (Code) references TicketCode(Code));
-        -- Define TicketNo as primary key -- 
-        -- Define StudentID as Forgiegn Key and Reference other table
-        -- Define Code as FK and References other table
-        
--- PART 3 -- Modify Tables
+-- -----------------------------------------------------
+-- Schema parkingspots
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `parkingspots` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `parkingspots` ;
 
-	alter table TicketCode
-		add column Description int;
-    alter table TicketCode
-		modify column Description varchar(25);
-	alter table Student
-		add column FinacialAid int;
-	alter table Student
-		drop column FinacialAid;
-        
--- PART 4 -- Modify Records 
+-- -----------------------------------------------------
+-- Table `parkingspots`.`rate`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `parkingspots`.`rate` (
+  `Start` INT NULL DEFAULT NULL,
+  `End` INT NULL DEFAULT NULL,
+  `Fee` INT NOT NULL,
+  `RateTimePeriod` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`Fee`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-	insert into Student(StudentID, FirstName, LastName, PhoneNum, StateLiceense, LicenseNo)
-		values (123, "Jake", "Yakonovich", 1234567890, "CO", 1234567980),
-			   (122, "Ugo", "Slavich", 1234567809, "WY", 1234568980);
-    insert into TicketCode (code, Fine)
-		values (1, 34),
-			   (2, 69);
-    insert into Ticket (TicketNo, Date,StudentID, Code)
-		values (1234, "10/06/2021", 00001, 1),
-			   (1234, "10/22/2021", 00002, 2);
-               
-    update TicketCode
-		set Fine = 50
-		where code=1;
-        -- Idenifes the first character in the first record updates 34 to 50
-               
--- PART 5 -- Queries
 
-	select code, fine from TicketCode where code=1 or 2;
-    
-	select TicketNo, Date
-    from Ticket
-    where StudentID=0001 
-    order by Date DESC;
-    
-    select StateLicense, LastName
-    from Student
-    where StateLicense="CO"
-    order by LastName ASC;
+-- -----------------------------------------------------
+-- Table `parkingspots`.`customers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `parkingspots`.`customers` (
+  `CustomerID` INT NOT NULL,
+  `FName` VARCHAR(30) NULL DEFAULT NULL,
+  `LName` VARCHAR(30) NULL DEFAULT NULL,
+  `Address` VARCHAR(30) NULL DEFAULT NULL,
+  `Email` VARCHAR(30) NULL DEFAULT NULL,
+  `Phone` VARCHAR(30) NULL DEFAULT NULL,
+  `Fee` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`CustomerID`),
+  INDEX `Fee` (`Fee` ASC) VISIBLE,
+  CONSTRAINT `customers_ibfk_1`
+    FOREIGN KEY (`Fee`)
+    REFERENCES `parkingspots`.`rate` (`Fee`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `parkingspots`.`address`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `parkingspots`.`address` (
+  `Street` VARCHAR(30) NULL DEFAULT NULL,
+  `City` VARCHAR(30) NULL DEFAULT NULL,
+  `State` VARCHAR(30) NULL DEFAULT NULL,
+  `Zip` INT NULL DEFAULT NULL,
+  `customers_CustomerID` INT NOT NULL,
+  INDEX `fk_address_customers1_idx` (`customers_CustomerID` ASC) VISIBLE,
+  CONSTRAINT `fk_address_customers1`
+    FOREIGN KEY (`customers_CustomerID`)
+    REFERENCES `parkingspots`.`customers` (`CustomerID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `parkingspots`.`spot`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `parkingspots`.`spot` (
+  `SpotID` INT NOT NULL,
+  `SpotNum` INT NULL DEFAULT NULL,
+  `SpotFloor` INT NULL DEFAULT NULL,
+  `SpotStat` VARCHAR(30) NULL DEFAULT NULL,
+  `customers_CustomerID` INT NOT NULL,
+  PRIMARY KEY (`SpotID`),
+  INDEX `fk_spot_customers1_idx` (`customers_CustomerID` ASC) VISIBLE,
+  CONSTRAINT `fk_spot_customers1`
+    FOREIGN KEY (`customers_CustomerID`)
+    REFERENCES `parkingspots`.`customers` (`CustomerID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `parkingspots`.`parking_event`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `parkingspots`.`parking_event` (
+  `SpotID` INT NULL DEFAULT NULL,
+  `Date` INT NULL DEFAULT NULL,
+  `Start` INT NULL DEFAULT NULL,
+  `End` INT NULL DEFAULT NULL,
+  `PicID` INT NOT NULL,
+  PRIMARY KEY (`PicID`),
+  INDEX `SpotID` (`SpotID` ASC) VISIBLE,
+  CONSTRAINT `parking_event_ibfk_1`
+    FOREIGN KEY (`SpotID`)
+    REFERENCES `parkingspots`.`spot` (`SpotID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
